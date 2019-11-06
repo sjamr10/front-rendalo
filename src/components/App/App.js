9990430f300729083d6643288dbcdd9200253f88
Sergio Miranda <smiranda@xsdev.tech>
2019-11-06 18:07:10 -0300

    Add code to show the people

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   const [hasError, setErrors] = useState(false);
-  const [people, setPeople] = useState({});
+  const [people, setPeople] = useState([]);
 
   async function fetchData() {
     const response = await fetch('http://localhost:3003/api/people');
@@ -20,8 +20,15 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">Rendalo Front</header>
-      <span>{JSON.stringify(people)}</span>
-      <hr />
+      <div>
+        {people.length === 0 ? "Don't forget to add some people first using the API" : ''}
+        {people.map(person => (
+          <div className="person">
+            <div className="data">{person.name}</div>
+            <div className="data">{person.age}</div>
+          </div>
+        ))}
+      </div>
       <span>{hasError ? 'Ha ocurrido un error.' : ''}</span>
     </div>
   );
