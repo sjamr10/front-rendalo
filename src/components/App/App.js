26afe9580dc76f33a8aae40586cc0b9bc148a012
Sergio Miranda <smiranda@xsdev.tech>
2019-11-06 17:25:30 -0300

    Add code to call the API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">Rendalo Front</header>
-  </div>
-);
+const App = () => {
+  const [hasError, setErrors] = useState(false);
+  const [people, setPeople] = useState({});
+
+  async function fetchData() {
+    const response = await fetch('http://localhost:3003/api/people');
+    response
+      .json()
+      .then(res => setPeople(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">Rendalo Front</header>
+      <span>{JSON.stringify(people)}</span>
+      <hr />
+      <span>{hasError ? 'Ha ocurrido un error.' : ''}</span>
+    </div>
+  );
+};
 
 export default App;
